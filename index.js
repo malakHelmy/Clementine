573f07fb32de1dbcfe06e3d8a906fce49065a700
fatimahelsayed <fatimahhatem3@gmail.com>
2023-05-08 23:27:20 +0300

    connected to a database
    established a connection with mongodb under the name of "clementine", username and password in .env

--- index.js
+++ index.js
@@ -1,49 +1,51 @@
-require('dotenv/config');
+require('dotenv/config')
 
-var express = require('express');
-const session = require('express-session');
-const ejs = require('ejs');
-const bodyparser = require('body-parser');
-const mongoose = require('mongoose');
-
-
+var express = require('express')
+const session = require('express-session')
+const ejs = require('ejs')
+const bodyparser = require('body-parser')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
 
 // http://localhost:8080/api/v1/products
-const api = process.env.API_URL;
-const app = express();
-const port = process.env.PORT || 8080;
+const api = process.env.API_URL
+const app = express()
+const port = process.env.PORT || 8080
 
 // middleware
-app.use(bodyparser.json());
-
-mongoose.connect(process.env.DB_URL, {
-    useNewURLParser: true,
-    useUnifiedTopology: true,
-})
-// const db = mongoose.connection;
-
-// db.on('error', (error) => console.log(error))
-// db.once('open', () => console.log('connected to database'))
+app.use(bodyparser.json())
+app.use(morgan('tiny')) //displays log requests
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
-
-app.listen(port, () => {
-    console.log(api);
-})
 app.use(bodyparser.urlencoded({ extended: true }))
 
 app.get(`${api}/products`, function (req, res) {
-
     const product = {
-        id: "1",
-        name: "example",
+        id: '1',
+        name: 'example',
     }
-    res.send(product);
+    res.send(product)
 })
 app.post(`${api}/products`, function (req, res) {
+    const newprod = req.body
+    console.log(newprod)
+    res.send(newprod)
+})
 
-    const newprod = req.body;
-    console.log(newprod);
-    res.send(newprod);
+mongoose
+    .connect(process.env.DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: 'clementine',
+    })
+    .then(() => {
+        console.log('database connection succeeded')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+app.listen(port, () => {
+    console.log(api)
 })
