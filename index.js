5de6333c3bb0503e66fbc27a1b3bfcf0ca89c153
fatimahelsayed <fatimahhatem3@gmail.com>
2023-05-08 21:22:16 +0300

    added API

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
-require('dotenv/config')
+require('dotenv/config');
 
-var express = require('express')
-const session = require('express-session')
-var ejs = require('ejs')
-var bodyparser = require('body-parser')
-const mongoose = require('mongoose')
+var express = require('express');
+const session = require('express-session');
+var ejs = require('ejs');
+var bodyparser = require('body-parser');
+const mongoose = require('mongoose');
 
 // http://localhost:8080/api/v1/products
-const app = express()
-const port = process.env.PORT || 8080
+const api = process.env.API_URL;
+const app = express();
+const port = process.env.PORT || 8080;
 
 mongoose.connect(process.env.DB_URL, {
     useNewURLParser: true,
     useUnifiedTopology: true,
 })
-const db = mongoose.connection
+const db = mongoose.connection;
+
 db.on('error', (error) => console.log(error))
 db.once('open', () => console.log('connected to database'))
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
-app.listen(port)
+app.listen(port, () => {
+    console.log(api);
+})
 app.use(bodyparser.urlencoded({ extended: true }))
 
 app.get('/', function (req, res) {
-    res.render('pages/index')
+    res.render('pages/index');
 })
