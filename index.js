d53f23f8990b9edd3ab5ea08dbc6a395af979669
fatimahelsayed <fatimahhatem3@gmail.com>
2023-05-08 18:31:47 +0300

    fixed index.ejs

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 8080;
 
-mongoose.connect(process.env.DB_URI, {userNewParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DB_URL, {useNewURLParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("connected to database"));
--- public/js/trial.js
+++ public/js/trial.js
@@ -1,4 +1,13 @@
 
+function myFunction() {
+    var x = document.getElementById("mynavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
+
 function newcollectionslider() {
     const prod_cont = [...document.querySelectorAll('.product-container')];
     const nxtBtn = [...document.querySelectorAll('.next_arrow')];
@@ -21,4 +30,25 @@ function newcollectionslider() {
         
     })
 }
+
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+    var reveals=document.querySelectorAll('.reveal');
+    for(var i=0;i<reveals.length;i++){
+        var windowheight=window.innerHeight;
+        var revealtop=reveals[i].getBoundingClientRect().top;
+        var revealpoint=150;
+
+        if(revealtop<windowheight-revealpoint){
+            reveals[i].classList.add('active');
+        }
+       
+     }
+ }
+
+ function closesidebar() {
+    document.getElementById("sidebar").style.display = "none";
+  }
+  
 
