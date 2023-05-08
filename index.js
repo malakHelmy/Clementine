de9483a686a6a598b129910ba1c9f93e59b25d8f
fatimahelsayed <106448000+fatimahelsayed@users.noreply.github.com>
2023-05-08 20:51:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-require("dotenv").config();
+require('dotenv/config')
 
-var express = require('express');
-const session = require("express-session");
-var ejs = require('ejs');
-var bodyparser=require('body-parser');
-const mongoose = require("mongoose");
-const app = express();
-const port = process.env.PORT || 8080;
+var express = require('express')
+const session = require('express-session')
+var ejs = require('ejs')
+var bodyparser = require('body-parser')
+const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DB_URL, {useNewURLParser: true, useUnifiedTopology: true});
-const db = mongoose.connection;
-db.on("error", (error) => console.log(error));
-db.once("open", () => console.log("connected to database"));
+// http://localhost:8080/api/v1/products
+const app = express()
+const port = process.env.PORT || 8080
 
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+mongoose.connect(process.env.DB_URL, {
+    useNewURLParser: true,
+    useUnifiedTopology: true,
+})
+const db = mongoose.connection
+db.on('error', (error) => console.log(error))
+db.once('open', () => console.log('connected to database'))
 
-app.listen(port);
-app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
 
-app.get('/',function(req,res){
-res.render('pages/index');
+app.listen(port)
+app.use(bodyparser.urlencoded({ extended: true }))
 
-});
+app.get('/', function (req, res) {
+    res.render('pages/index')
+})
