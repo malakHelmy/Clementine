36798d6b04ada6812183f515039ac1933b1939e8
fatimahelsayed <fatimahhatem3@gmail.com>
2023-05-08 22:32:22 +0300

    added middleware
    ensured that data is received in the backend

--- index.js
+++ index.js
@@ -2,23 +2,28 @@ require('dotenv/config');
 
 var express = require('express');
 const session = require('express-session');
-var ejs = require('ejs');
-var bodyparser = require('body-parser');
+const ejs = require('ejs');
+const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
+
+
 
 // http://localhost:8080/api/v1/products
 const api = process.env.API_URL;
 const app = express();
 const port = process.env.PORT || 8080;
 
+// middleware
+app.use(bodyparser.json());
+
 mongoose.connect(process.env.DB_URL, {
     useNewURLParser: true,
     useUnifiedTopology: true,
 })
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
-db.on('error', (error) => console.log(error))
-db.once('open', () => console.log('connected to database'))
+// db.on('error', (error) => console.log(error))
+// db.once('open', () => console.log('connected to database'))
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
@@ -28,6 +33,17 @@ app.listen(port, () => {
 })
 app.use(bodyparser.urlencoded({ extended: true }))
 
-app.get('/', function (req, res) {
-    res.render('pages/index');
+app.get(`${api}/products`, function (req, res) {
+
+    const product = {
+        id: "1",
+        name: "example",
+    }
+    res.send(product);
+})
+app.post(`${api}/products`, function (req, res) {
+
+    const newprod = req.body;
+    console.log(newprod);
+    res.send(newprod);
 })
