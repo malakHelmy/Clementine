c23140a7c42c4d2ada20ef174b7f7864bb87232d
fatimahelsayed <fatimahhatem3@gmail.com>
2023-05-10 13:23:20 +0300

    added semicolons

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-require('dotenv/config')
+require('dotenv/config');
 
-var express = require('express')
-const session = require('express-session')
-const ejs = require('ejs')
-const bodyparser = require('body-parser')
-const mongoose = require('mongoose')
-const morgan = require('morgan')
+var express = require('express');
+const session = require('express-session');
+const ejs = require('ejs');
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 // http://localhost:8080/api/v1/products
-const api = process.env.API_URL
-const app = express()
-const port = process.env.PORT || 8080
+const api = process.env.API_URL;
+const app = express();
+const port = process.env.PORT || 8080;
 
 // middleware
-app.use(bodyparser.json())
-app.use(morgan('tiny')) //displays log requests
+app.use(bodyparser.json());
+app.use(morgan('tiny')); //displays log requests
 
-app.use(express.static('public'))
-app.set('view engine', 'ejs')
-app.use(bodyparser.urlencoded({ extended: true }))
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyparser.urlencoded({ extended: true }));
 
 app.get(`${api}/products`, function (req, res) {
     const product = {
         id: '1',
         name: 'example',
-    }
-    res.send(product)
-})
+    };
+    res.send(product);
+});
 app.post(`${api}/products`, function (req, res) {
-    const newprod = req.body
-    console.log(newprod)
-    res.send(newprod)
-})
+    const newprod = req.body;
+    console.log(newprod);
+    res.send(newprod);
+});
 
 mongoose
     .connect(process.env.DB_URI, {
@@ -40,12 +40,11 @@ mongoose
         dbName: 'clementine',
     })
     .then(() => {
-        console.log('database connection succeeded')
+        console.log('database connection succeeded');
     })
     .catch((err) => {
-        console.log(err)
-    })
-
+        console.log(err);
+    });
 app.listen(port, () => {
-    console.log(api)
-})
+    console.log(api);
+});
