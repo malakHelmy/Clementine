80e0491e492a982dd2dd3a3e2003cbe9b5f11291
fatimahelsayed <fatimahhatem3@gmail.com>
2023-05-11 22:28:24 +0300

    updated product schema

--- models/product.js
+++ models/product.js
@@ -1,17 +1,57 @@
 const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    //turned an element to an object
-    //so that an error occurs if this element wasn't present while sending other elements of productSchema
+    id: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    images: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    material: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+
     countInStock: {
         type: Number,
         required: true,
+        min: 0,
+        max: 20,
+    },
+    featured: {
+        type: Boolean,
+        default: false,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
     },
 });
 
 /*model is the equivalent of 'collection' in node.js,
-'exports' will allow Product to be seen in any other file using the 'require' method,
+'exports' will allow Product to be seen in other files using the 'require' method,
 exporting method #1*/
 exports.Product = mongoose.model('products', productSchema);
