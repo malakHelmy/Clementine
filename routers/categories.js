eb6807aaeda3ee8e079d46d83f6177333b4c7351
fatimahelsayed <fatimahhatem3@gmail.com>
2023-05-13 23:23:11 +0300

    added edit/update
    also checks for valid category

--- routers/categories.js
+++ routers/categories.js
@@ -1,48 +1,88 @@
-
 const express = require('express');
-const {Category} = require('../models/category');
+const { Product } = require('../models/product');
+const { Category } = require('../models/category');
 const router = express.Router();
 
+//display all objects in the category schema
 router.get(`/`, async function (req, res) {
-
     const categories = await Category.find();
 
     //catching errors method #1
     if (!categories) {
         res.status(500).json({ success: false });
     }
-    res.send(categories);
-})
+    res.status(200).send(categories);
+});
 
+//display a specific object from the schema
+router.get('/:id', async (req, res) => {
+    const category = await Category.findById(req.params.id);
+
+    if (!category) {
+        return res.status(500).json({
+            message:
+                'The Category with the given ID was not found please check for the validity of the ID',
+        });
+    }
+
+    res.status(200).send(category);
+});
+
+//add an object to the schema
 router.post(`/`, async function (req, res) {
     let cat = new Category({
         id: req.body.id,
         name: req.body.name,
-    })
+    });
 
     //catching errors method #2
     cat = await cat.save();
-    if(!cat)
-    {
+    if (!cat) {
         return res.status(404).send('The Category cannot be created');
     }
 
     res.send(cat);
-})
+});
 
-router.delete('/:id', function (req,res) {
-    Category.findByIdAndRemove(req.params.id).then(category =>{
-        if(category){
-            return res.status(200).json({success: true, message: 'the category has been deleted'});
-        }
-        else 
-        {
-            return res.status(404).json({success: false, message: 'the category ia not found'});
-        }
-    }).catch(err=>{
-        return res.status(400).json({success: false, error: err});
+//edit or update an object from the schema
+router.put('/:id', async (req, res) => {
+    const category = await Category.findByIdAndUpdate(req.params.id, {
+        id: req.body.id,
+        name: req.body.name,
+    },
+    {
+        //return updated data
+        new: true
     });
-})
+
+    if(!category)
+    {
+        return res.status(404).send('The Category cannot be updated');
+    }
+
+    res.send(category);
+});
+
+//delete an object from the schema
+router.delete('/:id', function (req, res) {
+    Category.findByIdAndRemove(req.params.id)
+        .then((category) => {
+            if (category) {
+                return res.status(200).json({
+                    success: true,
+                    message: 'the category has been deleted',
+                });
+            } else {
+                return res.status(404).json({
+                    success: false,
+                    message: 'the category ia not found',
+                });
+            }
+        })
+        .catch((err) => {
+            return res.status(400).json({ success: false, error: err });
+        });
+});
 
 //exporting method #2
 module.exports = router;
--- routers/products.js
+++ routers/products.js
@@ -3,6 +3,7 @@ routers are responsible for creating/ storing/ Importing and exporting APIs betw
 */
 const express = require('express');
 const { Product } = require('../models/product');
+const { Category } = require('../models/category');
 const router = express.Router();
 
 router.get(`/`, async function (req, res) {
@@ -24,7 +25,12 @@ router.get(`/`, async function (req, res) {
     }
     res.send(prodList);
 });
-router.post(`/`, function (req, res) {
+router.post(`/`, async (req, res) => {
+    const cat = await Category.findById(req.body.category);
+    if (!cat) {
+        return res.status(400).send('This Category does not exist');
+    }
+
     const product = new Product({
         id: req.body.id,
         name: req.body.name,
@@ -38,6 +44,7 @@ router.post(`/`, function (req, res) {
         featured: req.body.featured,
         date: req.body.date,
     });
+
     //catching errors method #2
     product
         .save()
@@ -52,19 +59,38 @@ router.post(`/`, function (req, res) {
         });
 });
 
-router.delete('/:id', function (req,res) {
-    Product.findByIdAndRemove(req.params.id).then(product =>{
-        if(product){
-            return res.status(200).json({success: true, message: 'the product has been deleted'});
-        }
-        else 
-        {
-            return res.status(404).json({success: false, message: 'the product was not found'});
-        }
-    }).catch(err=>{
-        return res.status(400).json({success: false, error: err});
-    });
-})
+router.delete('/:id', function (req, res) {
+    Product.findByIdAndRemove(req.params.id)
+        .then((product) => {
+            if (product) {
+                return res.status(200).json({
+                    success: true,
+                    message: 'the product has been deleted',
+                });
+            } else {
+                return res.status(404).json({
+                    success: false,
+                    message: 'the product was not found',
+                });
+            }
+        })
+        .catch((err) => {
+            return res.status(400).json({ success: false, error: err });
+        });
+});
+
+router.get('/:id', async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(500).json({
+            message:
+                'The Product with the given ID was not found please check for the validity of the ID',
+        });
+    }
+
+    res.status(200).send(product);
+});
 
 //exporting method #2
 module.exports = router;
