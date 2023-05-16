065cae91f63583e9a0941505a0d8fa2d46443f93
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2023-05-16 17:44:55 +0300

    added bycrypt

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ require('dotenv/config');
 
 const express = require('express');
 const session = require('express-session');
+const fileUpload = require('express-fileupload');
 const ejs = require('ejs');
 const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const path = require('path');
+const fileUpload = require('express-fileupload');
+
 
 //Routes
 const productsRouter = require('./routers/products');
 const usersRouter = require('./routers/users');
 const categoriesRouter = require('./routers/categories');
 const ordersRouter = require('./routers/orders');
+const fileUpload = require('express-fileupload');
 
 // http://localhost:8080/api/v1/products
 const api = process.env.API_URL;
@@ -49,21 +53,56 @@ mongoose
         console.log(err);
     });
 
+app.use(express.urlencoded({extended: true}));
+
+app.use(fileUpload());
+app.use(session({ secret: 'Your_secret_key'}))
+
+
 app.get(`/`, function (req, res) {
-    res.render('pages/index');
+    res.render('pages/index', { user: (req.session.user === undefined ? "" : req.session.user)});
 });
 app.get(`/categories`, function (req, res) {
-    res.render('pages/categories');
+    res.render('pages/categories', { user: (req.session.user === undefined ? "" : req.session.user)});
 });
 app.get(`/userprofile`, function (req, res) {
-    res.render('pages/userprofile');
+    res.render('pages/userprofile', { user: (req.session.user === undefined ? "" : req.session.user)});
 });
 app.get(`/signup`, function (req, res) {
-    res.render('pages/signup');
+    res.render('pages/signup', { user: (req.session.user === undefined ? "" : req.session.user)});
 });
-app.get(`/dashboard`, function (req, res) {
-    res.render('pages/dashboard');
+app.get(`/dashboard`, (req, res) => {
+    if (req.session.user !== undefined) {
+        if (req.session.user.type === 'admin') {
+            Admins.find().then( result => {
+                res.render('pages/dashboard', {employees : result, user: (req.session.user === undefined ? "" : req.session.user)});
+
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+        else {
+            res.send("Access Denied.")
+        }
+    }
+    else {
+        res.send("Access Denied.")
+    }
 });
+
+app.post('/profile', (req, res) => {
+})
+
+
+app.post('/sign-up-action', (req,res)=>{
+
+})
+
+app.get('/logout', (req, res)) => {
+req.session.destroy();
+res.redirect('/');
+}
 app.listen(port, () => {
     console.log(api);
 });
