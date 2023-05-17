0d42c96f1bf6683a9531bdf4954aeda821cd5a4a
saheramr15 <saher2110197@miuegypt.edu.eg>
2023-05-17 03:09:19 +0300

    hashing the password

--- routers/users.js
+++ routers/users.js
@@ -1,11 +1,19 @@
 
 const express = require('express');
 const  User  = require('../models/user');
+const bcrypt=require('bcrypt')
 const router = express.Router();
 
-router.post(`/`,   (req, res) => {
-    const users=new User(req.body);
-   
+router.post(`/`, async  (req, res) => {
+
+        const user={
+          firstname:req.body.firstname,
+          lastname:req.body.lastname,
+          email:req.body.email,
+          password: await bcrypt.hash(req.body.password,12),
+          phone:req.body.phone
+        }
+        const users=new User(user);
     users
         .save( )
         .then( result => {
