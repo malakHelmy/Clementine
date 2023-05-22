df21db0b1895d414e65d345ed0941ea7aa749bfd
fatimahelsayed <fatimahhatem3@gmail.com>
2023-05-23 00:41:26 +0300

    product schema was missing some elements

--- models/product.js
+++ models/product.js
@@ -1,40 +1,54 @@
 const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema(
-  {
-    id: {
-      type: String,
-      required: true,
+    {
+        id: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        material: {
+            type: String,
+            required: true,
+        },
+        category: {
+            type: String,
+            required: true,
+        },
+        countInStock: {
+            type: Number,
+            required: true,
+            min: 0,
+            max: 10000,
+        },
+        featured: {
+            type: Boolean,
+            default: false,
+        },
+
+        date: {
+            type: Date,
+            default: Date.now,
+        },
     },
-    name: {
-      type: String,
-      required: true,
-    },
-    image: {
-      type: String,
-      required: true,
-    },
-    price: {
-      type: Number,
-      required: true,
-      default: 0,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    category: {
-      type: String,
-      required: true,
-    },
-    countInStock: {
-      type: Number,
-      required: true,
-      min: 0,
-      max: 10000,
-    },
-  },
-//   { _id: false } // Disable the default _id field
+    { timestamps: true }
+    //   { _id: false } // Disable the default _id field
 );
 
 // Set the 'id' field as the primary identifier
