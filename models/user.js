d5c7d757ab505dcf8f8351b61918455c9f22a35f
fatimahelsayed <fatimahhatem3@gmail.com>
2023-05-23 00:42:44 +0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const userSchema = new Schema({
-    firstname: String,
-    lastname: String,
-    email: String,
-    password: String,
-    phone: String,
-});
+const userSchema = new Schema(
+    {
+        firstname: String,
+        lastname: String,
+        email: String,
+        password: String,
+        phone: String,
+    },
+    { timestamps: true }
+);
 
 const user = mongoose.model('User', userSchema);
 module.exports = user;
