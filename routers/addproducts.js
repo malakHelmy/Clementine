b80c90eb86dd07494a731152369c68dacb7801d5
saheramr15 <saher2110197@miuegypt.edu.eg>
2023-05-26 20:28:23 +0300

    .

--- routers/addproducts.js
+++ routers/addproducts.js
@@ -3,6 +3,11 @@ const express = require('express');
 const { Product }  = require('../models/product');
 const router = express.Router();
 
+
+router.get('/',(req,res)=>{
+res.render('pages/addproducts')
+})
+
 router.post(`/`,   (req, res) => {
 
      
