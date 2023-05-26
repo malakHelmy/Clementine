0ecb0a3134b0d5cd764bb0d670538e392611c295
EslamAhmedv <125676587+EslamAhmedv@users.noreply.github.com>
2023-05-26 19:02:37 +0300

    delete buttn now can delete product from mongodb edit button now redirect to edit product page

--- routers/displayproducts.js
+++ routers/displayproducts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { Product } = require('../models/product');
 
+// Display all products
 router.get('/', async (req, res) => {
   try {
     // Fetch all products from the database
@@ -9,6 +10,23 @@ router.get('/', async (req, res) => {
 
     // Render the products template and pass the products data
     res.render('pages/displayproducts', { products });
+  } catch (error) {
+    console.log(error);
+    // Handle any errors that occur
+    res.render('pages/error');
+  }
+});
+
+// Delete a product
+router.post('/delete', async (req, res) => {
+  try {
+    const { productId } = req.body;
+
+    // Find the product by ID and remove it from the database
+    await Product.findByIdAndRemove(productId);
+
+    // Redirect back to the displayproducts page
+    res.redirect('/displayproducts');
   } catch (error) {
     console.log(error);
     // Handle any errors that occur
