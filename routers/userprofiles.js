762732986aa4e53b65cd9d6d72d99b115c1badd9
MazenFouad <125610746+MazenFouad@users.noreply.github.com>
2023-05-31 00:25:07 +0300

    Update userprofiles.js

--- routers/userprofiles.js
+++ routers/userprofiles.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const Order = require('../models/order');
+const {Order} = require('../models/order');
 //const { OrderItem } = require('../models/order-items');
 const router = express.Router();
+const User = require('../models/user');
 
 router.get(`/`, async (req, res) => {
 
@@ -18,5 +19,53 @@ router.get(`/`, async (req, res) => {
   });
 });
 
+function getLoggedInUserName(req) {
+  const userEmail = req.session.user;
+  if (!userEmail) {
+    return Promise.reject('User email not found in session');
+  }
 
+  return User.findOne({ email: userEmail })
+    .then((user) => {
+      if (user) {
+        return user.name;
+      }
+      return null;
+    })
+    .catch((error) => {
+      console.log('Error retrieving user from the database:', error);
+      throw new Error('Failed to retrieve user from the database');
+    });
+}
+
+router.get('/userprofile', async (req, res) => {
+  try {
+    const loggedInUserName = await getLoggedInUserName(req);
+
+    if (!loggedInUserName) {
+      res.status(401).send('Unauthorized');
+      return;
+    }
+
+    const user = await User.findOne({ email: req.session.user });
+
+    if (user) {
+      const wishlistItemCount = user.wishlist.length;
+
+      res.render('pages/userprofile', { user: loggedInUserName, wishlistItemCount }, (err, html) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send('Internal Server Error');
+        } else {
+          res.send(html);
+        }
+      });
+    } else {
+      res.status(404).send('User not found');
+    }
+  } catch (error) {
+    console.log('Error retrieving user:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 module.exports = router;
