d75f1e35d8979f84772ad2db2275be7f9fbfd535
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2023-06-01 01:31:43 +0300

    still fixing it

--- public/js/dashsearch.js
+++ public/js/dashsearch.js
@@ -1,6 +1,6 @@
 const search = document.querySelector('.search_orders input'),
-table_rows = document.querySelectorAll('tbody tr'),
-table_headings = document.querySelectorAll('thead th');
+    table_rows = document.querySelectorAll('tbody tr'),
+    table_headings = document.querySelectorAll('thead th');
 
 search.addEventListener('input', searchTable);
 
@@ -12,19 +12,20 @@ function searchTable() {
         row.classList.toggle('hide', table_data.indexOf(search_data) < 0);
         row.style.setProperty('--delay', i / 25 + 's');
     });
-    
+
     document.querySelectorAll('tbody tr:not(.hide)').forEach((visible_row, i) => {
         visible_row.style.backgroundColor = (i % 2 == 0) ? 'transparent' : '#0000000b';
     });
 }
-function showPopup(){
-            document.getElementById("popup-container").style.display = "block";
-        }
 
-        function hidePopup() {
-            document.getElementById("popup-container").style.display = "none";
-        }
-
-        function deleteCustomer() {
-            document.querySelector("form").submit();
-        }
+function showPopup(customerId) {
+    document.getElementById("customerId").value = customerId;
+    document.getElementById("popup-container").style.display = "block";
+}
+function hidePopup() {
+    document.getElementById("customerId").value = "";
+    document.getElementById("popup-container").style.display = "none";
+}
+function deleteCustomer(customerId) {
+    document.querySelector(`form[action="/editcustdash/${customerId}"]`).submit();
+}
