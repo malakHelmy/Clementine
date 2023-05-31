b99578399fac172d6f64b5d1e2228dcbbdc3b08f
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2023-05-31 13:19:54 +0300

    products validation

--- routers/addproducts.js
+++ routers/addproducts.js
@@ -1,27 +1,64 @@
 const express = require('express');
-const { Product } = require('../models/product');const router = express.Router();
+const { Product } = require('../models/product');
+const multer = require('multer');
+const router = express.Router();
+const { addProduct } = require('../controllers/addProductController');
+
+const productstorage = multer.diskStorage({
+  destination: 'public/Images',
+  filename: (req, file, myfunc) => {
+    myfunc(null, file.originalname); 
+  },
+});
+const upload = multer({ storage: productstorage }).array('photo', 6);
 
 router.get('/add', (req, res) => {
-  res.render('pages/addproducts');
+  const invalidInputs = {};
+  res.render('pages/addproducts', { invalidInputs });
 });
 
 router.get('/view', (req, res) => {
   res.render('pages/displayproducts');
 });
 
+router.get('/addproducts', (req, res) => {
+  const invalidInputs = {};
+  const files = req.files || [];
+  res.render('pages/addproducts', { invalidInputs, files });
+});
 
-router.post('/', (req, res) => {
-  const product = new Product(req.body);
+router.post('/addproducts', upload, async (req, res) => {
+  const { name, price, description, material, category, countInStock } = req.body;
+  const images = req.files.map(file => file.path);
 
-  product
-    .save()
-    .then(() => {
-      res.render('pages/displayproducts');
-    })
-    .catch((err) => {
-      console.log(err);
-      res.render('pages/addproducts');
-    });
+  const product = new Product({
+    name,
+    price,
+    description,
+    material,
+    category,
+    countInStock,
+    images,
+  });
+
+  try {
+    awaitproduct.save();
+    res.redirect('/view');
+  } catch (error) {
+    console.error(error);
+    res.render('pages/addproducts', { error: 'Failed to add product' });
+  }
+});
+
+router.post('/uploadfiles', (req, res) => {
+  upload(req, res, (error) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Failed to upload images');
+    } else {
+      res.render('pages/images', { files: req.files });
+    }
+  });
 });
 
 module.exports = router;
