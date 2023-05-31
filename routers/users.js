75b9cb111390ba13c3708749ed8d151691434c02
saheramr15 <saher2110197@miuegypt.edu.eg>
2023-05-31 17:53:30 +0300

    Update users.js

--- routers/users.js
+++ routers/users.js
@@ -1,51 +1,121 @@
-
 const express = require('express');
 const  User  = require('../models/user');
 const bcrypt=require('bcrypt')
 const router = express.Router();
-
 router.post(`/`, async  (req, res) => {
 
-        const user={
-          firstname:req.body.firstname,
-          lastname:req.body.lastname,
-          email:req.body.email,
-          password: await bcrypt.hash(req.body.password,12),
-          phone:req.body.phone
+  const firstnamevalue=req.body.firstname;
+  const lastnamevalue=req.body.lastname;
+  const passvalue=req.body.password;
+  const confirmpassvalue=req.body.confirmpassword;
+  const phonevalue=req.body.phone;
+  const emailvalue=req.body.email;
+  let c=0;
+  let Error={
+    firsterror:String,
+    lasterror:String,
+    emailerror:String,
+    passerror:String,
+    confirmpasserror:String,
+    phoneerror:String,
+  }
+  if(firstnamevalue=='')
+  {
+    Error.firsterror='Please enter Firstname';
+     c++;
+  }
+  
+  if(lastnamevalue=='')
+  {
+    Error.lasterror='Please enter Lastname';
+    c++;
+  }
+  else{
+
+  }
+  
+  if(emailvalue=='')
+  {
+    Error.emailerror='Please enter Email';
+    c++;
+  }
+  else{
+ 
+  } 
+   if(passvalue=='' )
+  {
+    Error.passerror='Please enter Password';
+    c++;
+  }
+  else{
+  }
+  if(confirmpassvalue==''){
+    Error.confirmpasserror='Please enter Confirm Password';
+    c++;
+  }
+  else if(confirmpassvalue!=passvalue)
+  {
+    Error.confirmpasserror='Please enter Matching Password';
+    c++;
+  }
+  else if(confirmpassvalue==passvalue)
+  {
+  }
+  if(phonevalue.length==11 )//typeof 37 === "number"; 
+  {
+
+  }
+  else
+  {
+    Error.phoneerror='Please enter Matching Password';
+    c++;
+  }
+ 
+        if(c==0)
+        {
+          const user={
+            firstname:req.body.firstname,
+            lastname:req.body.lastname,
+            email:req.body.email,
+            password: await bcrypt.hash(req.body.password,12),
+            phone:req.body.phone
+          }
+          const users=new User(user);
+         const check= User.findOne({email:req.body.email}).then((result)=>{
+            if(result==undefined)
+            {
+              users
+              .save()
+              .then( (result) => {
+                 
+                req.session.user=req.body.email;
+                if(req.session.cart!=undefined)
+                req.session.cart.items.forEach((items) => {
+                  items.email=req.session.user;
+                });  
+                res.redirect('/')
+              })
+              .catch( err => {
+                console.log(err);
+              });
+            } 
+            else{
+              res.render('pages/signup',{ user: req.session.user == undefined ? undefined : req.session.user,
+                cart: req.session.cart == undefined ? undefined : req.session.cart,})
+            }               
+         }).catch( err => {
+          console.log(err);
+        });
         }
-        const users=new User(user);
-        
-       const check= User.findOne({email:req.body.email}).then((result)=>{
-          if(result==undefined)
-          {
-            users
-            .save()
-            .then( (result) => {
-               
-              req.session.user=req.body.email;
-              if(req.session.cart!=undefined)
-              req.session.cart.items.forEach((items) => {
-                items.email=req.session.user;
-              });  
+        else{
+          res.render('pages/signup',{ user: req.session.user == undefined ? undefined : req.session.user,
+            cart: req.session.cart == undefined ? undefined : req.session.cart,})
+        }
+       
 
-              res.redirect('/')
-            })
-            .catch( err => {
-              console.log(err);
-            });
-          } 
-          else{
-            res.render('pages/signup',{ user: req.session.user == undefined ? undefined : req.session.user,
-              cart: req.session.cart == undefined ? undefined : req.session.cart,})
-          }               
-       }).catch( err => {
-        console.log(err);
-      });
 });
-
 router.post(`/checkemail`, async  (req, res) => {
   var query = { email: req.body.email };
-  console.log( req.body);
   User.find(query)
       .then(result => {
           if (result.length > 0) {
@@ -59,4 +129,6 @@ router.post(`/checkemail`, async  (req, res) => {
           console.log(err);
       });
 });
+
+
 module.exports = router;
