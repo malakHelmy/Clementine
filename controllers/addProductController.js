0e6f4c1d75c714bd319fe0c04f2901554a7da208
fatimahelsayed <fatimahhatem3@gmail.com>
2023-05-31 20:49:58 +0300

    add product + multiple images

--- controllers/addProductController.js
+++ controllers/addProductController.js
@@ -1,100 +1,163 @@
-exports.addProduct = async (req, res) => {
-    const { name, price, description, material, category, countInStock } = req.body;
-    let invalidInputs = {};
-    let files = req.files;
-    let images = [];
+const { basename } = require('path');
+const { Product } = require('../models/product');
+const user = require('../models/user');
+const asyncHandler = require('express-async-handler');
+const path = require('path');
 
-    if (!name || name.trim() === "") {
-        invalidInputs.name = "Invalid jewelry name, please try again.";
+exports.validateProduct = asyncHandler(async (req, res) => {
+    let imgFile;
+    let uploadPath;
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+    imgFile = req.files['img'][0];
+    uploadPath = path.join(req.body.id + path.extname(imgFile.name));
+
+    const { name, price, description, material, category, countInStock } =
+        req.body;
+    if (!name || name.trim() === '') {
+        invalidInputs.name = 'Invalid jewelry name, please try again.';
     }
 
-    if (!description || description.trim() === "") {
-        invalidInputs.description = "You must define the product.";
+    if (!description || description.trim() === '') {
+        invalidInputs.description = 'You must define the product.';
     }
 
-    if (!material || material.trim() === "") {
-        invalidInputs.material = "Please enter the material of the jewelry.";
+    if (!material || material.trim() === '') {
+        invalidInputs.material = 'Please enter the material of the jewelry.';
     }
 
-    if (!price || price.trim() === "") {
-        invalidInputs.price = "You must enter a price value.";
+    if (!price || price.trim() === '') {
+        invalidInputs.price = 'You must enter a price value.';
     }
 
     if (!/^\d+(.\d{1,2})?$/.test(price)) {
-        invalidInputs.price = "Invalid price input.";
+        invalidInputs.price = 'Invalid price input.';
     }
 
-    if (!category || category.trim() === "") {
-        invalidInputs.category = "Please enter the category.";
+    if (!category || category.trim() === '') {
+        invalidInputs.category = 'Please enter the category.';
     }
 
-    if (!countInStock || countInStock.trim() === "") {
-        invalidInputs.countInStock = "Please enter the count in stock.";
+    if (!countInStock || countInStock.trim() === '') {
+        invalidInputs.countInStock = 'Please enter the count in stock.';
     } else if (!/^\d+$/.test(countInStock)) {
-        invalidInputs.countInStock = "Count in stock must be a positive integer.";
-    }
-
-    if (!files || Object.keys(files).length === 0) {
-        invalidInputs.images = "Please upload at least one image.";
-    } else {
-        for (let file of files) {
-            let image = {
-                filename: file.filename,
-                path: file.path,
-            };
-            images.push(image);
-        }
+        invalidInputs.countInStock =
+            'Count in stock must be a positive integer.';
     }
 
     if (Object.keys(invalidInputs).length > 0) {
-        return res.status(400).json({ errors: invalidInputs });
+        return res.status(404).json({ errors: invalidInputs });
     }
 
-    try {
+    // Use the mv() method to place the file somewhere on your server
+    imgFile.mv(uploadPath, function (err) {
+        if (err) res.status(500).send(err);
+
         const product = new Product({
-            name,
-            price,
-            description,
-            material,
-            category,
-            countInStock,
-            images,
+            id: req.body.id,
+            name: req.body.name,
+            image: req.body.id,
+            images: req.body.id,
+            price: req.body.price,
+            description: req.body.description,
+            material: req.body.material,
+            category: req.body.category,
+            countInStock: req.body.countInStock,
         });
-        const savedProduct = await product.save();
-        res.status(201).json(savedProduct);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send("Server Error");
+        product
+            .save()
+            .then((result) => {
+                res.redirect('/addproducts');
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    });
+});
+
+exports.addProduct = asyncHandler(async function (req, res, next) {
+    const { id, name, price, description, material, category, countInStock } =
+        req.body;
+    let imgFiles = [];
+    let uploadPaths = [];
+
+    if (Array.isArray(req.files.img)) {
+        imgFiles = [...req.files.img];
+    } else {
+        imgFiles.push(req.files.img);
     }
-};
-
-async function addProduct(req, res, next) {
-    const { name, price, description, material, category, countInStock } = req.body;
-    const images = req.files['photo'].map(file => file.path); // access the array of files using the field name
-
-    console.log('Files:', req.files); // checking if the files are being uploaded
-    console.log('Paths:', images); // checking if the paths are being saved correctly
+    let imageFile;
+    let i = 0;
+    imgFiles.forEach((imgFile) => {
+        imageFile = imgFile.path;
+        if (i < 4) {
+            imageFile = path.basename(imageFile);
+            uploadPaths.push(imageFile);
+            i++;
+        }
+    });
+    let image = req.files['img'][0].path;
+    image = path.basename(image);
 
     const product = new Product({
+        id,
         name,
+        image,
+        images: uploadPaths,
         price,
         description,
         material,
         category,
         countInStock,
-        images,
+        // save the uploaded image path to the database
     });
+    product
+        .save()
+        .then((result) => {
+            res.redirect('/view');
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
 
-    try {
-        await product.save();
-        res.redirect('/view');
-    } catch (error) {
-        console.error(error);
-        const invalidInputs = { name, price, description, material, category, countInStock };
-        const files = req.files || [];
-        res.render('pages/addproducts', { error: 'Failed to add product', invalidInputs, files });
-    }
-}
+// async function addProduct(req, res, next) {
+//     const { name, price, description, material, category, countInStock } =
+//         req.body;
+//     const images = req.files['photo'].map((file) => file.path); // access the array of files using the field name
 
-module.exports = { addProduct };
+//     console.log('Files:', req.files); // checking if the files are being uploaded
+//     console.log('Paths:', images); // checking if the paths are being saved correctly
 
+//     const product = new Product({
+//         name,
+//         price,
+//         description,
+//         material,
+//         category,
+//         countInStock,
+//         images,
+//     });
+
+//     try {
+//         await product.save();
+//         res.redirect('/view');
+//     } catch (error) {
+//         console.error(error);
+//         const invalidInputs = {
+//             name,
+//             price,
+//             description,
+//             material,
+//             category,
+//             countInStock,
+//         };
+//         const files = req.files || [];
+//         res.render('pages/addproducts', {
+//             error: 'Failed to add product',
+//             invalidInputs,
+//             files,
+//         });
+//     }
+// }
--- routers/addproducts.js
+++ routers/addproducts.js
@@ -2,38 +2,38 @@ const express = require('express');
 const { Product } = require('../models/product');
 const multer = require('multer');
 const router = express.Router();
-const { addProduct } = require('../controllers/addProductController');
+const products = require('../controllers/addProductController');
 
 const productstorage = multer.diskStorage({
-  destination: 'public/Images',
-  filename: (req, file, myfunc) => {
-    myfunc(null, file.originalname);
-  },
+    destination: 'public/Images',
+    filename: (req, file, myfunc) => {
+        myfunc(null, file.originalname);
+    },
 });
 
 const upload = multer({
   storage: productstorage,
   fileFilter: (req, file, cb) => {
-    if (file.fieldname === 'photo') {
-      cb(null, true);
-    } else {
-      cb(new Error('Invalid field name'), false);
-    }
+      if (file.fieldname === 'img') {
+          cb(null, true);
+      } else {
+          cb(new Error('Invalid field name'), false);
+      }
   },
-}).fields([{ name: 'photo', maxCount: 6 }]);
+}).fields([{ name: 'img', maxCount: 4 }]);
 
 router.get('/view', (req, res) => {
-  res.render('pages/displayproducts');
+    res.render('pages/displayproducts');
 });
 
 router.get('/', (req, res) => {
-  const invalidInputs = {};
-  const images = []; // intializing an empty array of images
-  res.render('pages/addproducts', {
-    invalidInputs, images
-  }); // passing the images array to ejs
+    let invalidInputs = {};
+    let images = []; // intializing an empty array of images
+    res.render('pages/addproducts', {
+        invalidInputs,
+        images,
+    }); // passing the images array to ejs
 });
 
-router.post('/', upload, addProduct);
-
+router.post('/', upload, products.addProduct);
 module.exports = router;
