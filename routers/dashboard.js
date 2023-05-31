cab38fb42fba394ed4d0dffd9b39d2d9f750139a
EslamAhmedv <125676587+EslamAhmedv@users.noreply.github.com>
2023-05-31 23:00:45 +0300

    real Total number of users appears on dashboard

--- routers/dashboard.js
+++ routers/dashboard.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const { Order } = require('../models/order');
+const User = require('../models/user');
 
 // Fetching recent orders
 router.get('/', async (req, res) => {
-  try {
-    const orders = await Order.find().sort({ createdAt: -1 }).limit(3);
-    res.render('pages/dashboard', { currentPage: 'dashboard', recentOrders: orders });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-});
+    try {
+      const totalUsers = await User.countDocuments();
+      const orders = await Order.find().sort({ createdAt: -1 }).limit(3);
+      res.render('pages/dashboard', { currentPage: 'dashboard', totalUsers: totalUsers, recentOrders: orders });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+  
 
 module.exports = router;
