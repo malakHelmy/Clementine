5462030256c286790604a67a9718c47288770d8c
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2023-06-02 17:18:48 +0300

    fixing bugs

--- routers/orders.js
+++ routers/orders.js
@@ -3,19 +3,33 @@ const { Order } = require('../models/order');
 const { User } = require('../models/user');
 const { OrderItem } = require('../models/order-items');
 const router = express.Router();
+const { Product } = require('../models/product');
+
 
  //adding a new order to the schema
  router.post(`/`, async (req, res) => {
     const orderItemsIds = Promise.all(
         //.map ashan andna array of order items
+
         req.body.orderItems.map(async (orderItem) => {
+            const product = await Product.findById(orderItem.product).select('name');
             let newOrderItem = new OrderItem({
-                quantity: orderItem.quantity,
-                product: orderItem.product,
+              quantity: orderItem.quantity,
+              product: orderItem.product,
+              name: product.name
             });
             newOrderItem = await newOrderItem.save();
             return newOrderItem._id;
-        })
+          })
+        
+        // req.body.orderItems.map(async (orderItem) => {
+        //     let newOrderItem = new OrderItem({
+        //         quantity: orderItem.quantity,
+        //         product: orderItem.product,
+        //     });
+        //     newOrderItem = await newOrderItem.save();
+        //     return newOrderItem._id;
+        // })
     );
 
     const orderItemIdsresolved = await orderItemsIds;
