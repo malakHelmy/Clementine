87d9acd1b501cee3b4940a435b92597fc9e18453
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2023-06-04 21:05:22 +0300

    ajax delete order

--- routers/orders.js
+++ routers/orders.js
@@ -6,20 +6,20 @@ const router = express.Router();
 const { Product } = require('../models/product');
 
 
- //adding a new order to the schema
- router.post(`/`, async (req, res) => {
+//adding a new order to the schema
+router.post(`/`, async (req, res) => {
     const orderItemsIds = Promise.all(
         //.map ashan andna array of order items
         req.body.orderItems.map(async (orderItem) => {
             const product = await Product.findById(orderItem.product).select('name');
             let newOrderItem = new OrderItem({
-              quantity: orderItem.quantity,
-              product: orderItem.product,
-              name: product.name
+                quantity: orderItem.quantity,
+                product: orderItem.product,
+                name: product.name
             });
             newOrderItem = await newOrderItem.save();
             return newOrderItem._id;
-          })
+        })
         // req.body.orderItems.map(async (orderItem) => {
         //     let newOrderItem = new OrderItem({
         //         quantity: orderItem.quantity,
@@ -32,7 +32,7 @@ const { Product } = require('../models/product');
 
     const orderItemIdsresolved = await orderItemsIds;
     let add_order = {
-        
+
         orderItems: orderItemIdsresolved,
         order_id: req.body.order_id,
         userID: req.body.userID,
@@ -48,13 +48,13 @@ const { Product } = require('../models/product');
     const orders = new Order(add_order);
     //catching errors method #2
     orders
-    .save()
-    .then((result) =>{
-        res.render('/ordersdash');
-    }) 
-    .catch((err) => {
-        console.log(err);
-    })
+        .save()
+        .then((result) => {
+            res.render('/ordersdash');
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 });
 
 
@@ -63,56 +63,56 @@ const { Product } = require('../models/product');
 router.get(`/`, async (req, res) => {
 
     Order.find()
-    .then(async (orderslist) => {          
-    res.render('pages/ordersdash', {
-      order: orderslist
-    })
+        .then(async (orderslist) => {
+            res.render('pages/ordersdash', {
+                order: orderslist
+            })
 
-    })
-    .catch((err) => {
-        
-      console.log(err);
-    });
-  });
+        })
+        .catch((err) => {
 
- 
+            console.log(err);
+        });
+});
 
-router.get('/:id', (req, res) =>{
+
+
+router.get('/:id', (req, res) => {
     Order.findById(req.params.id)
-   // .populate('userID', '_id')
-    .then((result) => {
-      res.render('pages/updateorder', {
-        viewTitle:'Update Order',
-        order: result
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  });
+        // .populate('userID', '_id')
+        .then((result) => {
+            res.render('pages/updateorder', {
+                viewTitle: 'Update Order',
+                order: result
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
 
 
-  router.post('/:id', async(req, res) => {
+router.delete('/:id', async (req, res) => {
     try {
         const orderID = req.params.id;
-        await Order.findOneAndDelete({_id: orderID});
+        await Order.findOneAndDelete({ _id: orderID });
         console.log(orderID);
 
         if (req.query.ajax)
-        return res.json({ message: "Order deleted successfully" });
-      }
-    catch(error) {
+            return res.json({ message: "Order deleted successfully" });
+    }
+    catch (error) {
         console.log('Error deleting order: ', error);
         res.status(500)
-        .json({ error: 'Failed to delete customer, please try again.' });
+            .json({ error: 'Failed to delete customer, please try again.' });
     }
-    })
+});
 
 //editing and updating status
 router.post('/:id/update', async (req, res) => {
-  
-    try{
-        const orderID=req.params.id;
+
+    try {
+        const orderID = req.params.id;
         const updates = req.body;
         await Order.findByIdAndUpdate(orderID, updates);
         res.redirect('/ordersdash');
@@ -123,10 +123,9 @@ router.post('/:id/update', async (req, res) => {
     }
 
 
-   
+
 
 });
-    
 
 
 
@@ -145,80 +144,81 @@ router.post('/:id/update', async (req, res) => {
 
 
 
-  /* router.get(`/`, async (req, res) => {
- //.populate -> if i want to know the user who ordered
-    //displays the first and last name for the user + sort by date from newest to oldest
-    const ordersList = await Order.find()
-        .populate('userID', 'firstname + lastname')
-        .sort({ dateOrdered: -1 })
-    .then(async (ordersList) => {          
-    res.render('pages/editcustdash', {
-      order: ordersList
-    })
 
-    })
-    .catch((err) => {
-        
-      console.log(err);
-    });
-  
+/* router.get(`/`, async (req, res) => {
+//.populate -> if i want to know the user who ordered
+  //displays the first and last name for the user + sort by date from newest to oldest
+  const ordersList = await Order.find()
+      .populate('userID', 'firstname + lastname')
+      .sort({ dateOrdered: -1 })
+  .then(async (ordersList) => {          
+  res.render('pages/editcustdash', {
+    order: ordersList
+  })
+
+  })
+  .catch((err) => {
+      
+    console.log(err);
   });
+ 
+});
 
 
 
 
 //to display a specific object (order id, etc) from db:
 
- router.get('/:id', async (req, res) => {
-    //populate to see order details
-    const orders = await Order.findById(req.params.id).populate({
-        path: 'orderItems',
+router.get('/:id', async (req, res) => {
+  //populate to see order details
+  const orders = await Order.findById(req.params.id).populate({
+      path: 'orderItems',
 
-        //populate will be an object to get all info about product items
-        populate: {
-            path: 'product',
-            populate: 'category',
-        },
-    });
+      //populate will be an object to get all info about product items
+      populate: {
+          path: 'product',
+          populate: 'category',
+      },
+  });
 
-    if (!orders) {
-        return res.status(500).json({
-            message: 'The order with the given ID was not found.',
-        });
-    }
+  if (!orders) {
+      return res.status(500).json({
+          message: 'The order with the given ID was not found.',
+      });
+  }
 
-    res.status(200).send(orders);
+  res.status(200).send(orders);
 });
 
 
 
 /* router.post(`/`, function (req, res) {
-    const order = new Order({
-        order_id: req.body.order_id,
-        userID: req.body.userID,
-        userName: req.body.userName,
-        orderitems: req.body.orderitems,
-        shippingAddress1: req.body.shippingAddress1,
-        shippingAddress2: req.body.shippingAddress2,
-        city: req.body.city,
-        zip: req.body.zip,
-        status: req.body.status,
-        totalAmount: req.body.totalAmount,
-        phone_num: req.body.phone_num,
-        dateOrdered: req.body.dateOrdered,
-    });
-    //catching errors method #2
-    order
-        .save()
-        .then((createdOrder) => {
-            res.status(201).json(createdOrder);
-        })
-        .catch((err) => {
-            res.status(500).json({
-                error: err,
-                success: false,
-            });
-        });
+  const order = new Order({
+      order_id: req.body.order_id,
+      userID: req.body.userID,
+      userName: req.body.userName,
+      orderitems: req.body.orderitems,
+      shippingAddress1: req.body.shippingAddress1,
+      shippingAddress2: req.body.shippingAddress2,
+      city: req.body.city,
+      zip: req.body.zip,
+      status: req.body.status,
+      totalAmount: req.body.totalAmount,
+      phone_num: req.body.phone_num,
+      dateOrdered: req.body.dateOrdered,
+  });
+  //catching errors method #2
+  order
+      .save()
+      .then((createdOrder) => {
+          res.status(201).json(createdOrder);
+      })
+      .catch((err) => {
+          res.status(500).json({
+              error: err,
+              success: false,
+          });
+      });
 });
 */
 /* router.delete('/:id', function (req, res) {
