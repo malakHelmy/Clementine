ca629773391d80c3c038aea20677d4130585ca65
saheramr15 <saher2110197@miuegypt.edu.eg>
2023-06-05 22:35:08 +0300

    Update logout.js

--- routers/logout.js
+++ routers/logout.js
@@ -1,25 +1,39 @@
 const express = require('express');
 const session = require('express-session');
-const Cartt = require('../models/cart');
+const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    
     if (req.session.cart != undefined) {
-        req.session.cart.items.forEach(async (items) => {
-            await Cartt.findOneAndUpdate(
-                { email: req.session.user },
+          
+
+                if(req.session.cart.items=='')
                 {
-                    id: items.id,
-                    name: items.name,
-                    image: items.image,
-                    price: items.price,
-                    quantity: items.quantity,
-                    email: req.session.user,
-                },
-                { upsert: true }
-            );
-        });
+                    console.log('cart is empty');
+                    await User.findOneAndUpdate(
+                        { email: req.session.user },
+                        {
+                           cart:[]
+                        },
+                        { upsert: true }
+                    );
+                }else{
+                    await User.findOneAndUpdate(
+                        { email: req.session.user },
+                        {
+                           cart:req.session.cart.items
+                        },
+                        { upsert: true }
+                    );
+
+                }
+
+
+
+            
+       
     }
     req.session.destroy();
     res.redirect('/');
