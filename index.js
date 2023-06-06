a111a8372f4890c91871c0f7ac83ffb01a3d3199
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2023-06-06 05:13:28 +0300

    bgrb haga

--- index.js
+++ index.js
@@ -283,38 +283,34 @@ app.post('/sign-up-action', (req, res) => { });
 
 app.post(`/contactus`, function (req, res) {
     // res.render('pages/contactus');
+    if (!req.session.isAuthenticated) {
+        return res.status(401).send('Unauthorized');
+      }
+    
+      var fullname = req.body.name;
+      var uemail = req.body.email;
+      var subject = req.body.subject;
+      var message = req.body.message;
+      var transporter = nodemailer.createTransport(req.body.transport);
 
-    var fullname = req.body.name;
-    var uemail = req.body.email;
-    var subject = req.body.subject;
-    var message = req.body.message;
-
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'clementineco2023@gmail.com',
-            pass: 'lmkwmjbyftpuzwhz',
-        },
-    });
-
-    var mailOptions = {
+      var mailOptions = {
         from: uemail,
         to: 'clementineco2023@gmail.com',
         subject: subject,
         text: message,
-    };
-
-    transporter.sendMail(mailOptions, function (error, info) {
+      };
+    
+      transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
-            console.log(error);
-            res.send('Error.');
+          console.log(error);
+          res.send('Error.');
         } else {
-            console.log('Email sent:' + info.response);
-            res.send('Successfully sent.');
+          console.log('Email sent:' + info.response);
+          res.send('Successfully sent.');
         }
         res.redirect('/');
+      });
     });
-});
 /* ---------CONTACT US FORM MAILER END --------*/
 
 app.use((req,res)=>{
