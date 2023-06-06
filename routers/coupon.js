d8397e29d5f243bc5e0e25f43700365f5d05caf5
MazenFouad <Mazenfouad02@gmail.com>
2023-06-06 18:52:47 +0300

    Update coupon.js

--- routers/coupon.js
+++ routers/coupon.js
@@ -5,6 +5,7 @@ const coupons = require('../controllers/couponController');
 router.post('/', coupons.createCoupon);
 router.get('/', coupons.getAllCoupons);
 router.put('/:id', coupons.updateCoupon);
+router.put('/:id', coupons.deleteCoupon);
 
 
 module.exports = router;
