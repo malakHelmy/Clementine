d5cdee37bc8a094eba086411ea7e8565cf5d6b28
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2023-06-07 02:47:03 +0300

    back to mail old code ashan mailgun karar yeamly disable

--- routers/mailController.js
+++ routers/mailController.js
@@ -12,35 +12,60 @@ router.post('/', async (req, res) => {
   
     const { name, subject, email, message } = req.body;
     console.log("email is " + req.body.email);
-  
-    const auth = {
-      // service: 'gmail',
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
       auth: {
-        api_key: '6ac800157976f73359d3af4c549a77ae-6d1c649a-58b339a8',
-        domain: 'sandboxe19a2e0ab2154778abca9d2d22bf130d.mailgun.org'   
-         }
-    };
-    const transporter = nodemailer.createTransport(mailgunTransport(auth));
-
+        user: 'clementineco2023@gmail.com',
+        pass: 'lmkwmjbyftpuzwhz'
+      }
+    });
   
     const mailOptions = {
       from: req.body.email,
       to: 'clementineco2023@gmail.com',
       subject: `Message from ${req.body.email}: ${subject}`,
-      text: `${message}`
+      text: `Name: ${name}\n${message}`
     };
-  
+
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
         console.log(error);
         res.status(500).send('Error.');
       } else {
         console.log('Email sent:' + info.response);
-        res.redirect('/');
+        res.redirect('/contactus');
         // res.status(200).send('Successfully sent.');
       }
     });
   })
+    // const auth = {
+    //   // service: 'gmail',
+    //   auth: {
+    //     api_key: '6ac800157976f73359d3af4c549a77ae-6d1c649a-58b339a8',
+    //     domain: 'sandboxe19a2e0ab2154778abca9d2d22bf130d.mailgun.org'   
+    //      }
+    // };
+    // const transporter = nodemailer.createTransport(mailgunTransport(auth));
+
+  
+    // const mailOptions = {
+    //   from: req.body.email,
+    //   to: 'clementineco2023@gmail.com',
+    //   subject: `Message from ${req.body.email}: ${subject}`,
+    //   text: `${message}`
+    // };
+  
+    // transporter.sendMail(mailOptions, (error, info) => {
+    //   if (error) {
+    //     console.log(error);
+    //     res.status(500).send('Error.');
+    //   } else {
+    //     console.log('Email sent:' + info.response);
+    //     res.redirect('/');
+    //     // res.status(200).send('Successfully sent.');
+    //   }
+    // });
+  
     // var transporter = nodemailer.createTransport({
     //     service: 'gmail',
     //     auth: {
