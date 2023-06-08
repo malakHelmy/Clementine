b708f093fae2eb5808f1b4c060e2035d8d51cf36
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2023-06-08 22:07:19 +0300

    minor validation edit

--- routers/editproducts.js
+++ routers/editproducts.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 const Product = require('../models/product').Product;
+const products = require('../controllers/addProductController');
+const path = require('path');
 
 // GET route for displaying the edit product form
+const productstorage = multer.diskStorage({
+    destination: 'public/Images',
+    filename: (req, file, myfunc) => {
+        myfunc(null, file.originalname);
+    },
+}); 
+
+const upload = multer({
+    storage: productstorage,
+    fileFilter: (req, file, cb) => {
+        if (file.fieldname === 'img') {
+            cb(null, true);
+        } else {
+            cb(new Error('Invalid field name'), false);
+        }
+    },
+}).fields([{ name: 'img', maxCount: 4 }]);
+
+
 router.get('/:id', async (req, res) => {
     try {
         if (req.session.admin != undefined) {
+            let invalidInputs = {};
+            let images = []; // intializing an empty array of images
             const productId = req.params.id;
             const product = await Product.findOne({ id: productId });
             if (product) {
@@ -26,8 +50,11 @@ router.get('/:id', async (req, res) => {
 });
 
 // POST route for updating the product
-router.post('/:id', async (req, res) => {
+router.post('/:id', upload, async (req, res) => {
     try {
+        
+        let invalidInputs = {};
+        let images = []; // intializing an empty array of images
         const productId = req.params.id;
         const updatedProductData = req.body;
         const updatedProduct = await Product.findOneAndUpdate(
@@ -46,5 +73,11 @@ router.post('/:id', async (req, res) => {
         res.status(500).send('Internal Server Error');
     }
 });
+
+// router.post('/', products.addProduct);
+
+
+
+
 
 module.exports = router;
